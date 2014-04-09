cf2139c79596ad6062e5f8d5660139ac5623bc1c
Jose R. Borja <chakaitos@gmail.com>
2014-04-09 16:40:11 -0500

    finish selection sort

--- sort.rb
+++ sort.rb
@@ -1,6 +1,22 @@
 module Sort
 
   def self.selection_sort(array)
+  	arr = array.clone
+  	n = arr.size
+	  if arr.size <= 1
+	    return arr
+	  end
+	  n.times do |index|
+      min_index = index
+
+	    (index+1).upto(n-1) do |i|
+	    	if arr[i] < arr[min_index]
+	    		min_index = i
+	    	end
+	    end
+	    arr[index], arr[min_index] = arr[min_index], arr[index]
+	  end
+	  return arr
   end
 
 end
