dcc787d9bc1cf6cc7ba7d1d1b38653a7b2676df0
Jose R. Borja <chakaitos@gmail.com>
2014-04-11 14:28:29 -0500

    Add specs for merge_sort and merge methods

--- spec/sort_spec.rb
+++ spec/sort_spec.rb
@@ -29,4 +29,75 @@ describe 'Sort' do
       expect(Sort.selection_sort(array)).to eq(sorted_array)
     end
   end
+
+  describe '.merge' do
+    context 'when the arguments are empty' do
+      it "returns an empty array" do
+        expect(Sort.merge([], [])).to eq([])
+      end
+    end
+
+    context 'when one of the arguments is empty' do
+      it "returns the other array" do
+        a = [5, 7, 9]
+        expect(Sort.merge(a, [])).to eq(a)
+        expect(Sort.merge([], a)).to eq(a)
+      end
+    end
+
+    context 'when each array has 1 element' do
+      it "returns an array with both elements sorted" do
+        expect(Sort.merge([5], [2])).to eq([2, 5])
+      end
+    end
+
+    context 'when you have 2 large sorted arrays' do
+      it "returns a sorted array that is the combination of the two" do
+        a = [2, 6, 10, 26, 58, 89, 103]
+        b = [1, 5, 11, 13, 15, 19, 60, 61, 62]
+        expect(Sort.merge(a, b)).to eq((a+b).sort)
+      end
+    end
+  end
+
+  describe '.mergesort' do
+    context 'when the argument is empty' do 
+      it "returns an empty array" do
+        expect(Sort.mergesort([])).to eq([])
+      end
+    end
+
+    context 'when the argument has 1 number' do 
+      it "returns the same array" do
+        expect(Sort.mergesort([1])).to eq([1])
+      end
+    end
+
+    context 'when the argument has sorted elements' do 
+      it "returns the same array" do
+        expect(Sort.mergesort([1, 2, 3])).to eq([1, 2, 3])
+      end
+    end
+
+    context 'when the argument has unsorted elements' do 
+      it "returns a sorted array" do
+        expect(Sort.mergesort([2, 1, 3])).to eq([1, 2, 3])
+      end
+    end
+
+    context 'when the argument has repeating elements' do 
+      it "returns a sorted array" do
+        expect(Sort.mergesort([3, 2, 3])).to eq([2, 3, 3])
+      end
+    end
+
+    context 'when the argument is large and randomized' do 
+      it "returns a sorted array" do
+        large_array = []
+        100.times {large_array << rand(1000)}
+        sorted_array = large_array.sort
+        expect(Sort.mergesort(large_array)).to eq(sorted_array)
+      end
+    end
+  end
 end
